5eeb2b9189f10d6e10f4831029c10e1121eddfaf
Josh Pollock <josh@Josh-Pollocks-MacBook-Pro.local>
2013-03-01 00:53:09 -0500

    put authors list in columns

--- lib/jp-functions.php
+++ lib/jp-functions.php
@@ -165,37 +165,36 @@ $authors = $wpdb->get_results("SELECT ID, user_nicename from $wpdb->users WHERE 
 foreach ($authors as $author ) {
 
 echo "<li>";
-echo "<a href=\"".get_bloginfo('url')."/author/";
-the_author_meta('user_nicename', $author->ID);
-echo "/\">";
-echo get_avatar($author->ID);
-echo "</a>";
-echo '<div>';
-echo "<a href=\"".get_bloginfo('url')."/author/";
-the_author_meta('user_nicename', $author->ID);
-echo "/\">";
-the_author_meta('display_name', $author->ID);
-echo "</a>";
-echo "<br />";
-echo "Website: <a href=\"";
-the_author_meta('user_url', $author->ID);
-echo "/\" target='_blank'>";
-the_author_meta('user_url', $author->ID);
-echo "</a>";
-echo "<br />";
-echo "Twitter: <a href=\"http://twitter.com/";
-the_author_meta('twitter', $author->ID);
-echo "\" target='_blank'>";
-the_author_meta('twitter', $author->ID);
-echo "</a>";
-echo "<br />";
-echo "<a href=\"".get_bloginfo('url')."/author/";
-the_author_meta('user_nicename', $author->ID);
-echo "/\">Visit&nbsp;";
-the_author_meta('display_name', $author->ID);
-echo "'s Profile Page";
-echo "</a>";
-echo "</div>";
+	echo '<div class="three columns">';
+		echo "<a href=\"".get_bloginfo('url')."/author/";
+		the_author_meta('user_nicename', $author->ID);
+		echo "/\">";
+		echo get_avatar($author->ID);
+		echo "</a>";
+	echo "</div>";
+	echo '<div class="nine columns">';
+		echo "<a href=\"".get_bloginfo('url')."/author/";
+		the_author_meta('user_nicename', $author->ID);
+		echo "/\">";
+		the_author_meta('display_name', $author->ID);
+		echo "</a>";
+		echo "<br />";
+		echo "Website: <a href=\"";
+		the_author_meta('user_url', $author->ID);
+		echo "/\" target='_blank'>";
+		the_author_meta('user_url', $author->ID);
+		echo "</a>";
+		echo "<br />";
+		
+		
+		
+		echo "<a href=\"".get_bloginfo('url')."/author/";
+		the_author_meta('user_nicename', $author->ID);
+		echo "/\">Visit&nbsp;";
+		the_author_meta('display_name', $author->ID);
+		echo "'s Profile Page";
+		echo "</a>";
+	echo "</div>";
 echo "</li>";
 }
 }
