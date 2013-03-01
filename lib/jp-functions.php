478d23bb51ba7de4fa31c77cb40254c8c2320b56
Josh Pollock <josh@Josh-Pollocks-MacBook-Pro.local>
2013-03-01 01:09:15 -0500

    added conditional social links in author-temp (NOT WORKING RIGHT)

--- lib/jp-functions.php
+++ lib/jp-functions.php
@@ -174,23 +174,41 @@ echo "<li>";
 	echo "</div>";
 	echo '<div class="nine columns">';
 		echo '<div class="row">';
-			echo "<a href=\"".get_bloginfo('url')."/author/";
-			the_author_meta('user_nicename', $author->ID);
-			echo "/\">";
+			echo "<a href=\"";
+			the_author_meta('user_url', $author->ID);
+			echo "/\" target='_blank'>";
 			the_author_meta('display_name', $author->ID);
 			echo "</a>";
 			echo "<br />";
-			echo "Website: <a href=\"";
+			echo "<a href=\"";
 			the_author_meta('user_url', $author->ID);
 			echo "/\" target='_blank'>";
-			the_author_meta('user_url', $author->ID);
+			echo "Website";
 			echo "</a>";
 			echo "<br />";
 		echo '</div>';
 		echo '<div class="row">';
-		
+			$google_profile = get_the_author_meta( 'google_profile' );
+			if ( $google_profile ) {
+			echo '<a class="fc-webicon googleplus small" href="' . esc_url($google_profile) . '" rel="me"></a>';
+			}
+			
+			$username = get_the_author_meta('tumblr');
+			if ( $username != null ) {
+			$a1 = "<a class=\"fc-webicon tumblr small\" href=\"http://";
+			$a2 = ".tumblr.com\" ></a>";
+			echo $a1.$username.$a2;
+			}
+			
+			$username = get_the_author_meta('twitter');
+			$a1 = "<a class=\"fc-webicon twitter small\" href=\"http://twitter.com/";
+			$a2 = "\"></a>";
+			if ( $username != null ) {
+			echo $a1.$username.$a2;
+			}
+			
 		echo '</div>';
-		echo 'div class="row">':
+		echo '<div class="row">';
 			//echo "<a href=\"".get_bloginfo('url')."/author/";
 			//the_author_meta('user_nicename', $author->ID);
 			//echo "/\">Visit&nbsp;";
