a4daf024cf36800c2e2707cb670cc9a37547c90f
Josh Pollock <josh@Josh-Pollocks-MacBook-Pro.local>
2013-03-01 19:21:39 -0500

    sidebar panel callout radius

--- sidebar.php
+++ sidebar.php
@@ -11,7 +11,7 @@
 ?>
 
 <!-- Sidebar -->
-<aside class="three columns">
+<aside class="three columns panel callout radius">
 
 <?php if ( dynamic_sidebar('Sidebar Right') ) : elseif( current_user_can( 'edit_theme_options' ) ) : ?>
 
