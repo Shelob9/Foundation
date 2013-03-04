035902985faa25cc7ad2e99fd70280d4576bf763
Josh Pollock <josh@Josh-Pollocks-MacBook-Pro.local>
2013-03-03 23:47:42 -0500

    testing…

--- content.php
+++ content.php
@@ -12,6 +12,7 @@
 
 <article>
 
+
 	<header>
 		<hgroup>
 			<h2><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'foundation' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
